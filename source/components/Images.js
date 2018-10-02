f160dcf33c0ed24677c54aedea95cbe4e8d33b97
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-02 20:11:53 +0300

    [92m→ Checkpoint at[39m: [34m20:46:14[39m;

--- source/components/Images.js
+++ source/components/Images.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
-export default class Header extends React.Component {
+export default class Images extends React.Component {
     render() {
         return (
-            <div className = 'header'>
-                <div className = 'logo' />
-                <input
-                    placeholder = 'Search'
-                    type = 'text'
-                />
-                <div className = 'nav'>
-                    <div className = 'explore' />
-                    <div className = 'likes' />
-                    <div className = 'settings' />
-                </div>
+            <div className = 'images'>
+                images
             </div>
         );
     }
