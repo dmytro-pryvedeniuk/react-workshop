7ad1825a61e2a5d5ece71d12a0de70c7e95eb665
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-02 20:11:53 +0300

    [92m→ Checkpoint at[39m: [34m20:47:49[39m;

---
+++ source/components/Images.1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Images extends React.Component {
+    render() {
+        return (
+            <div className = 'images'>
+                images
+            </div>
+        );
+    }
+}
