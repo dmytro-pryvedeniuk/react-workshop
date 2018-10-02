38b1038c387cacbbc7d1f5fbde9bac12c7c131b7
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-02 20:11:53 +0300

    [92m→ Checkpoint at[39m: [34m20:39:06[39m;

--- source/components/Content.js
+++ source/components/Content.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Profile from './Profile';
 
 export default class Content extends React.Component {
     render() {
         return (
             <div className = 'content'>
-                <div>Profile</div>
+                <Profile />
                 <div>Images</div>
             </div>
         );
